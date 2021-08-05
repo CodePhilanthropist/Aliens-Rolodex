f509b0559e12cc423205709e2909aae1d68c4e38
sampe <barrigaete@gmail.com>
2021-08-05 19:53:00 +0800

    reverting back to brackets instread of parenthesis

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const CardList = props => {
     <div>
         {
-            props.posts.map(pic=>(
+            props.posts.map(pic=>{
                 return (<h1>{pic.title}</h1>);
-            ))
+            })
         }
     </div>
 }
