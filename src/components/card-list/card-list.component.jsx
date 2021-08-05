f48dc1e7696be9ffd3f090600120ada0374a96bc
sampe <barrigaete@gmail.com>
2021-08-05 19:55:31 +0800

    adding class nae

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const CardList = props => {
-    <div>
+    <div className="card-list">
         {
             props.children.map(pic=>{
                 return (<h1>{pic.title}</h1>);
