29678dda358c77fb9dde2f8e6752294d0565b6b1
sampe <barrigaete@gmail.com>
2021-08-05 19:51:57 +0800

    changing from baces to parenthesis

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const CardList = props => {
     <div>
         {
-            props.posts.map(pic=>{
+            props.posts.map(pic=>(
                 <h1>{pic.title}</h1>
-            })
+            ))
         }
     </div>
 }
