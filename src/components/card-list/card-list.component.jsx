b418c580bfdb715853953c647d3c331755c9dc32
sampe <barrigaete@gmail.com>
2021-08-06 23:44:38 +0800

    adding props

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const CardList = props => {
     <div className="card-list">
         {
-            props.children.map(pic=>{
+            props.posts.map(pic=>{
                 return (<h1>{pic.title}</h1>);
             })
         }
