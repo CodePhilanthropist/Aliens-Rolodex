49eccdb700ef77b43e8af784267712c97d154f3b
CodePhilanthropist <barrigaete@gmail.com>
2021-08-07 17:44:25 +0800

    adding a callback function to onChange parameter of input

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-
-import './App.css';
-import React, { Component } from 'react';
-import { CardList } from './components/card-list/card-list.component';
+import "./App.css";
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: "",
+    };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-       <CardList monsters={this.state.monsters}/> 
-       
+        <input
+          type="search"
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value }, () => console.log(this.state.searchField));
+          }}
+        />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
