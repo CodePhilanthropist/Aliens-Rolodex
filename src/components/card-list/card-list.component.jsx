2d1a52a3d47c2b5bfd9871ec9142b91b462c7b52
sampe <barrigaete@gmail.com>
2021-08-07 13:59:33 +0800

    insert card inside the list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Card} from '..card.component';
+import {Card} from '../card/card.component';
 
 import './card-list.style.css';
 
 export const CardList = props => {
     return <div className="card-list">
-        <Card monster={props.monsters}/>
-        {props.monsters.map(pic => <h1>{pic.name}</h1>)}
+        
+        {props.monsters.map(monster => <Card key={monster.id}/>)}
     </div>
 }
 
