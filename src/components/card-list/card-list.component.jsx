463e87458bf8de696e5266071fd8172ecd46ed08
sampe <barrigaete@gmail.com>
2021-08-07 13:44:58 +0800

    changes posts to monsters

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,7 @@ import './card-list.style.css';
 
 export const CardList = props => {
     return <div className="card-list">
-        {props.posts.map(pic => <h1>{pic.name}</h1>)}
+        {props.monsters.map(pic => <h1>{pic.name}</h1>)}
     </div>
 }
 
