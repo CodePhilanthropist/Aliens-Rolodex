f99ad66a9957e79db37f72a40bf75ef7e6e94bcc
sampe <barrigaete@gmail.com>
2021-08-07 13:53:59 +0800

    adding h1 to represent card name

--- src/components/card/component.card.jsx
+++ src/components/card/component.card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Card = props => {
     return (
         <div className="card">
-            
+            <h1>{props.monster.name}</h1>
         </div>
     );
 }
