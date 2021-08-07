b6068669b77609ac4a2124e9cdb0646708a833fb
sampe <barrigaete@gmail.com>
2021-08-07 14:08:11 +0800

    adding pic

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,8 @@ import './card.style.css';
 export const Card = props => {
     return (
         <div className="card-container">
-            <h1>{props.monster.name}</h1>
+            <img alt={props.monster.id} src={`https://robohash.org/${props.monster.id}/?set=set2&size=150x150`}/>
+            <h2>{props.monster.name}</h2>
             <p>{props.monster.email}</p>
         </div>
     );
