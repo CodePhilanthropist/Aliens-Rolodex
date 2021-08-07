1ab13a061aa0b88b31e8ce7c644a15c4cd34a172
sampe <barrigaete@gmail.com>
2021-08-07 13:57:24 +0800

    import card component to card-list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Card} from './card/card.component';
+import {Card} from '..card.component';
 
 import './card-list.style.css';
 
 export const CardList = props => {
     return <div className="card-list">
+        <Card monster={props.monsters}/>
         {props.monsters.map(pic => <h1>{pic.name}</h1>)}
     </div>
 }
