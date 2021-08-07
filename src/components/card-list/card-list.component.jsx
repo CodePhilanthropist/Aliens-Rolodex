6434289d33f59d1401be57b91b3d016dd674474b
sampe <barrigaete@gmail.com>
2021-08-07 13:25:25 +0800

    changes in my heart

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 export const CardList = props => {
     <div className="card-list">
         {
-            props.posts.map(pic=>{
-                return console.log("check if ok");
-            })
+            
+           
         }
     </div>
 }
