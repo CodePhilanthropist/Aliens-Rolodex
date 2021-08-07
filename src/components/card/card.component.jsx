5f75a3e00d10639f2e23eb18e4ca275921260ac8
sampe <barrigaete@gmail.com>
2021-08-07 14:04:00 +0800

    renaming the card classname

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import './card.style.css';
 
 export const Card = props => {
     return (
-        <div className="card">
+        <div className="card-container">
             <h1>{props.monster.name}</h1>
         </div>
     );
