d7eeaa2796e654d8fbe59c22084e3d68b5a1e902
sampe <barrigaete@gmail.com>
2021-08-07 13:30:11 +0800

    adding children to components

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <CardList name="Rian"/>
+       <CardList name="Rian"> This is just a text output</CardList>
        {this.state.posts.map(pic => <h1>{pic.title}</h1>)}
       </div>
     );
